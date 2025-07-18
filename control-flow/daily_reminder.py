7cce4aa3df54ce554f762f9fd9c054f9c07ae1fa
Calvin Mensah <calvinmensah@Calvins-MacBook-Pro.local>
2025-07-18 15:53:57 +0100

    submission: 4. Personal Daily Reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,24 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").strip().lower()
+time_bound = input("Is it time-bound? (yes/no): ").strip().lower()
+
+message = ""
+
+match priority:
+    case "high":
+        message += f"'Reminder: {task}' is a high priority task"
+    case "medium":
+        message += f"Note: '{task}' is a medium priority task"
+    case "low":
+        message += f"Note: '{task}' is a low priority task"
+    case _:
+        print("Invalid priority level.")
+        exit()
+        
+if time_bound == "yes":
+    message += " that requires immediate attention today!"
+else:
+    message += ". Consider completing it when you have free time."
+
+
+print(message)
