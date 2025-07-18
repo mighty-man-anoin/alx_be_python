dbca51392e418dcd1bca59bc5333a679630f19a2
Calvin Mensah <calvinmensah@Calvins-MacBook-Pro.local>
2025-07-18 15:57:52 +0100

    fixes:

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,7 +6,7 @@ message = ""
 
 match priority:
     case "high":
-        message += f"'Reminder: {task}' is a high priority task"
+        message += f"Reminder: '{task}' is a high priority task"
     case "medium":
         message += f"Note: '{task}' is a medium priority task"
     case "low":
